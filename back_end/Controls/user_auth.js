877f6229be169caba2836a945bcb2b82c55c398d
Aymsep <71574687+Aymsep@users.noreply.github.com>
2023-02-23 02:15:43 +0100

    auth ready , login and register work perfectly and connected to backend , still need to integrate database

--- back_end/Controls/user_auth.js
+++ back_end/Controls/user_auth.js
@@ -1,9 +1,80 @@
-exports.login = (req, res, next) => {
-    console.log('login')
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+const users = []
+
+exports.login = async (req, res, next) => {
+    const user_find = await users.find(user => user.email == req.body.email)    
+    console.log(user_find)
+    if(user_find == null) {
+        return res.status(404).send({
+            message:'user not found'
+        })
+    }
+    try{
+        const check_match = bcrypt.compare(req.body.email, user_find.password)
+        if(check_match){
+            return res.send({
+                message: 'password match'
+            })
+        }else{
+            return res.send({
+                message: 'password not match'
+            })
+        }
+
+    }catch{
+        res.status(500).send()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+
+
+   
+   
+   
+   
+    
 }
 
-exports.register = (req, res, next) => {
-    console.log(req.body)
-    res.status(200).json(req.body)
-    
+
+
+
+
+
+
+
+
+exports.register = async (req, res, next) => {
+   try{
+    const hashedpass = await bcrypt.hash(req.body.password,10)
+    const user  = {
+        id:uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedpass
+    }
+    res.status(201).send({
+        message: 'user created succedfully'
+       })
+       users.push(user)
+   }catch{
+    res.status(500).json({message: 'user not created'})
+   }
+  
+}
+
+
+exports.getusers =  (req, res, next) => {
+    res.status(200).send(users)
 }
--- back_end/Routes/user_auth.js
+++ back_end/Routes/user_auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router  = express.Router()
 const auth = require('../Controls/user_auth')
 
-router.post('/auth',auth.register)
-
+router.post('/register',auth.register)
+router.get('/user',auth.getusers)
+router.post('/login',auth.login)
 
 module.exports = router
